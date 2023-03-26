1faefc258e9d21d5c27199c82068166c46f52102
titto <tit.tolk07022003@gmail.com>
2023-03-27 00:55:41 +0700

    Разметка для первого фрагмента главного экрана

---
+++ app/src/main/java/com/example/worldcinema/ui/compilation/CompilationViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.worldcinema.ui.compilation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CompilationViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is compilation Fragment"
+    }
+    val text: LiveData<String> = _text
+}
