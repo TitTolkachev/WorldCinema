fbc0b0854733256e575d2a3a4d64f4a8aee344ca
titto <tit.tolk07022003@gmail.com>
2023-03-28 02:26:01 +0700

    Верстка экрана профиля

--- app/src/main/java/com/example/worldcinema/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/worldcinema/ui/profile/ProfileFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.worldcinema.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -26,13 +25,8 @@ class ProfileFragment : Fragment() {
             ViewModelProvider(this)[ProfileViewModel::class.java]
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
