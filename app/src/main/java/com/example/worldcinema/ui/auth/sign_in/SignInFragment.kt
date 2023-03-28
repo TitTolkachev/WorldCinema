9f350b7dd997e76f9d2b21dc1ba9d1c8cdc4994e
titto <tit.tolk07022003@gmail.com>
2023-03-28 21:34:49 +0700

    Навигация из Auth в Main

--- app/src/main/java/com/example/worldcinema/ui/auth/sign_in/SignInFragment.kt
+++ app/src/main/java/com/example/worldcinema/ui/auth/sign_in/SignInFragment.kt
@@ -1,15 +1,18 @@
 package com.example.worldcinema.ui.auth.sign_in
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.worldcinema.R
 import com.example.worldcinema.databinding.FragmentSignInBinding
+import com.example.worldcinema.ui.main.MainActivity
+
 
 class SignInFragment : Fragment() {
 
@@ -32,7 +35,11 @@ class SignInFragment : Fragment() {
         navController = findNavController()
 
         binding.buttonEnter.setOnClickListener {
-            navController.navigate(R.id.action_signInFragment_to_homeFragment)
+            val intent = Intent(view?.context, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
         binding.buttonRegistration.setOnClickListener {
             navController.navigate(R.id.action_signInFragment_to_signUpFragment)
