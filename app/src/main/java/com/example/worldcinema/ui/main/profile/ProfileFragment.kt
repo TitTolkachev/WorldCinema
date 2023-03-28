9e01e4d6e5f532b2ad811917a954da1b6645770d
titto <tit.tolk07022003@gmail.com>
2023-03-28 23:19:16 +0700

    Поправка шрифтов на кнопках

--- app/src/main/java/com/example/worldcinema/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/worldcinema/ui/main/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.worldcinema.ui.main.profile
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.worldcinema.databinding.FragmentProfileBinding
+import com.example.worldcinema.ui.auth.AuthActivity
 
 class ProfileFragment : Fragment() {
 
@@ -25,6 +27,15 @@ class ProfileFragment : Fragment() {
             ViewModelProvider(this)[ProfileViewModel::class.java]
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.buttonExit.setOnClickListener {
+            val intent = Intent(view?.context, AuthActivity::class.java)
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
 
         return binding.root
     }
