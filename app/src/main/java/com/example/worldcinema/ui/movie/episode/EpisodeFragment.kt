4452205f7ec64b7cdf5bec6105a7baf8a676a826
titto <tit.tolk07022003@gmail.com>
2023-04-02 20:51:00 +0700

    Плеер Episode Screen в процессе

--- app/src/main/java/com/example/worldcinema/ui/movie/episode/EpisodeFragment.kt
+++ app/src/main/java/com/example/worldcinema/ui/movie/episode/EpisodeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.worldcinema.ui.movie.episode
 
-import androidx.lifecycle.ViewModelProvider
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.worldcinema.databinding.FragmentEpisodeBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerView
 
 class EpisodeFragment : Fragment() {
 
@@ -21,6 +27,10 @@ class EpisodeFragment : Fragment() {
 
     private lateinit var viewModel: EpisodeViewModel
 
+    private lateinit var videoView: PlayerView
+    private lateinit var exoPlayer: ExoPlayer
+    private lateinit var videoProgressBar: ProgressBar
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,10 +41,38 @@ class EpisodeFragment : Fragment() {
 
         navController = findNavController()
 
+        videoView = binding.videoView
+        videoProgressBar = binding.progressBarVideo
+
+        exoPlayer = ExoPlayer.Builder(requireContext()).build()
+        videoView.player = exoPlayer
+        videoView.keepScreenOn = true
+        exoPlayer.addListener(object : Player.Listener {
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                super.onPlayerStateChanged(playWhenReady, playbackState)
+            }
+        })
+        val videoSource = Uri.parse("https://drive.google.com/uc?export=view&id=1ra6f2jGr6Jja8UXptmpGDHO9ERiMmE6d")
+        val mediaItem = MediaItem.fromUri(videoSource)
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
+        exoPlayer.play()
+
         return binding.root
     }
 
+    override fun onStop() {
+        exoPlayer.stop()
+        super.onStop()
+    }
+
+    override fun onPause() {
+        exoPlayer.pause()
+        super.onPause()
+    }
+
     override fun onDestroyView() {
+        exoPlayer.stop()
         _binding = null
         super.onDestroyView()
     }
