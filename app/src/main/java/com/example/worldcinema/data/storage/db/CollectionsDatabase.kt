68acef0a95a6e058a0384d1882f3497bb980c737
titto <tit.tolk07022003@gmail.com>
2023-04-16 19:08:25 +0700

    Отображение иконок коллекций

--- app/src/main/java/com/example/worldcinema/data/storage/db/CollectionsDatabase.kt
+++ app/src/main/java/com/example/worldcinema/data/storage/db/CollectionsDatabase.kt
@@ -3,7 +3,7 @@ package com.example.worldcinema.data.storage.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [], version = 1)
+@Database(entities = [CollectionIcon::class], version = 1)
 abstract class CollectionsDatabase : RoomDatabase() {
     abstract fun collectionIconDao(): CollectionIconDao
 }
--- app/src/main/java/com/example/worldcinema/ui/screen/collection/icon/IconCollectionActivity.kt
+++ app/src/main/java/com/example/worldcinema/ui/screen/collection/icon/IconCollectionActivity.kt
@@ -2,7 +2,11 @@ package com.example.worldcinema.ui.screen.collection.icon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.core.view.get
 import androidx.lifecycle.ViewModelProvider
+import com.example.worldcinema.R
 import com.example.worldcinema.databinding.ActivityIconCollectionBinding
 
 class IconCollectionActivity : AppCompatActivity() {
@@ -20,6 +24,78 @@ class IconCollectionActivity : AppCompatActivity() {
             finish()
         }
 
+        inflateIcons()
+
         setContentView(binding.root)
+    }
+
+    private fun inflateIcons() {
+
+        val icons = listOf(
+            R.drawable.ic_1,
+            R.drawable.ic_2,
+            R.drawable.ic_3,
+            R.drawable.ic_4,
+            R.drawable.ic_5,
+            R.drawable.ic_6,
+            R.drawable.ic_7,
+            R.drawable.ic_8,
+            R.drawable.ic_9,
+            R.drawable.ic_10,
+            R.drawable.ic_11,
+            R.drawable.ic_12,
+            R.drawable.ic_13,
+            R.drawable.ic_14,
+            R.drawable.ic_15,
+            R.drawable.ic_16,
+            R.drawable.ic_17,
+            R.drawable.ic_18,
+            R.drawable.ic_19,
+            R.drawable.ic_20,
+            R.drawable.ic_21,
+            R.drawable.ic_22,
+            R.drawable.ic_23,
+            R.drawable.ic_24,
+            R.drawable.ic_25,
+            R.drawable.ic_26,
+            R.drawable.ic_27,
+            R.drawable.ic_28,
+            R.drawable.ic_29,
+            R.drawable.ic_30,
+            R.drawable.ic_31,
+            R.drawable.ic_32,
+            R.drawable.ic_33,
+            R.drawable.ic_34,
+            R.drawable.ic_35,
+            R.drawable.ic_36,
+        )
+        icons.forEachIndexed { index, i ->
+            when (index % 4) {
+                0 -> {
+                    layoutInflater.inflate(R.layout.collection_icon, binding.FlexboxIcons1)
+                    val view = binding.FlexboxIcons1[binding.FlexboxIcons1.childCount - 1]
+                    view.id = View.generateViewId()
+                    (view as ImageView).setImageResource(i)
+                }
+                1 -> {
+                    layoutInflater.inflate(R.layout.collection_icon, binding.FlexboxIcons2)
+                    val view = binding.FlexboxIcons2[binding.FlexboxIcons2.childCount - 1]
+                    view.id = View.generateViewId()
+                    (view as ImageView).setImageResource(i)
+                }
+                2 -> {
+                    layoutInflater.inflate(R.layout.collection_icon, binding.FlexboxIcons3)
+                    val view = binding.FlexboxIcons3[binding.FlexboxIcons3.childCount - 1]
+                    view.id = View.generateViewId()
+                    (view as ImageView).setImageResource(i)
+                }
+                3 -> {
+                    layoutInflater.inflate(R.layout.collection_icon, binding.FlexboxIcons4)
+                    val view = binding.FlexboxIcons4[binding.FlexboxIcons4.childCount - 1]
+                    view.id = View.generateViewId()
+                    (view as ImageView).setImageResource(i)
+                }
+            }
+        }
     }
 }
